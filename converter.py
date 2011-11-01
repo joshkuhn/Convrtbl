7c418ba639b6fb5d5b6d21acad2d4a9d2e8f8bf2
Josh Kuhn <josh.kuhn@gmail.com>
2011-11-01 16:11:53 -0600

    Added two new currencies, refactored class hierarchy slightly

--- converter.py
+++ converter.py
@@ -4,6 +4,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+
 form = """<form action="/convert" method="post">
                 <h2>Currency Converter</h2>
                 <div>
@@ -11,12 +12,16 @@ form = """<form action="/convert" method="post">
                     <select name="from">
                         <option selected>USD</option>
                         <option>Euro</option>
+                        <option>Rupee</option>
                         <option>Yuan</option>
+                        <option>Yen</option>                        
                     </select> to                     
                     <select name="to">
                         <option>USD</option>
-                        <option selected>Yuan</option>
                         <option>Euro</option>
+                        <option>Rupee</option>
+                        <option selected>Yuan</option>
+                        <option>Yen</option>
                     </select>
                     <input type="submit" value="Convert" />
                 </div>                
@@ -27,7 +32,45 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write('<html><body>'+ form + '</body></html>')
 
 
-class Guestbook(webapp.RequestHandler):
+class Converter(webapp.RequestHandler):    
+    extab = {
+        'Euro': 0.714,
+        'Yuan': 6.35,
+        'Yen': 75.89,
+        'Rupee': 49.5,
+        'USD': 1
+    }
+
+    def validateInput(self, quantity, fromCurrency, toCurrency):
+        """
+        This function checks types and ranges to avoid invalid input
+        """
+        if not self.extab.has_key(fromCurrency):
+            return False
+        if not self.extab.has_key(toCurrency):
+            return False
+        if quantity is None:
+            return False
+        try:
+            float(quantity)
+        except:
+            # could not parse a float out of the quantity string.
+            return False
+
+        return True
+
+    def convert(self, quantity, fromCurrency, toCurrency):
+        """ 
+        all currencies are converted to the base currency (USD) first
+        then to the target currency.  For this reason, our lookup table
+        only needs conversions from 1 USD.
+        """
+        
+        baseValue = quantity * self.extab[fromCurrency]
+        finalValue = baseValue * self.extab[toCurrency]
+            
+        return finalValue
+
     def post(self):
         self.response.out.write('<html><body>')
 
@@ -36,8 +79,8 @@ class Guestbook(webapp.RequestHandler):
         toCurrency = self.request.get('to')
 
         # only try the conversion if the user's input validates.
-        if (validateInput(quantityIn, fromCurrency, toCurrency)):
-            quantityOut = convert(float(quantityIn), fromCurrency, toCurrency)
+        if (self.validateInput(quantityIn, fromCurrency, toCurrency)):
+            quantityOut = self.convert(float(quantityIn), fromCurrency, toCurrency)
             self.response.out.write(form + '<br>')
     
             self.response.out.write('<h3>' + str(quantityIn) + ' ' + fromCurrency)
@@ -51,45 +94,10 @@ class Guestbook(webapp.RequestHandler):
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/convert', Guestbook)],
+                                      ('/convert', Converter)],
                                      debug=True)
 
-def validateInput(quantity, fromCurrency, toCurrency):
-    """
-    This function checks types and ranges to avoid invalid input
-    """
-    supportedCurs = ['Euro', 'USD', 'Yuan']
-    if fromCurrency not in supportedCurs:
-        return False
-    if toCurrency not in supportedCurs:
-        return False
-    if quantity is None:
-        return False
-    try:
-        float(quantity)
-    except:
-        # could not parse a float out of the quantity string.
-        return False
 
-    return True
-
-def convert(quantity, fromCurrency, toCurrency):
-    """ 
-    all currencies are converted to the base currency (USD) first
-    then to the target currency.  For this reason, our lookup table
-    only needs conversions from 1 USD.
-    """
-        
-    extab = {
-    'Euro': 0.714,
-    'Yuan': 6.35,
-    'USD': 1
-    }
-    
-    baseValue = quantity * extab[fromCurrency]
-    finalValue = baseValue * extab[toCurrency]
-        
-    return finalValue
 
 def main():
     run_wsgi_app(application)
